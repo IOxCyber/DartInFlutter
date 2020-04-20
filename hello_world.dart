fc5bfb07bab1161ba0c8bed8116e2f51e47e177c
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2020-04-20 13:27:09 +0530

    Update hello_world.dart

--- hello_world.dart
+++ hello_world.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Center(
-    child: Text("Hello Flutter", textDirection: TextDirection.ltr),
-  ));
+  runApp(
+    new MaterialApp(
+    debugShowCheckedModeBanner:false,  
+    title: "My Flutter App",
+    home: new Material(
+        color: Colors.green,
+        child: Center(
+            child: Text(
+              "Hello Flutter", 
+              textDirection: TextDirection.ltr,
+              style:TextStyle(color:Colors.white,fontSize:50.0),
+              ),
+        ),
+    ),
+  )
+  );
 }
