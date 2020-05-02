fb519ced900377b5f2226fa17e9aa6c72053d94a
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2020-05-02 23:56:44 +0530

    Update Mixins_IN.dart

--- Mixins_IN.dart
+++ Mixins_IN.dart
@@ -38,3 +38,14 @@ class Swallow extends Bird {
     print('eating a mosquito');
   }
 }
+
+
+/*
+1. Mixin is a way to reuse a class's code in multiple class heirarchy.
+2. Mixins allow you to plug in blocks of code without needing to create subclasses.
+3. Dart support multiple Mixins.
+4. If the class contains any identical methods to mixins or super class then the methods in the present 
+class would be called first always.(if there is mixins called as A,B,C & a class Zee uses all A,B,C &
+all three have identical methods suppose methodX then class Zee will call the most super class's method)
+5. It's simply declaring a Method(using Mixin) & use it "with" keyword within the class without inherit it.
+*/
