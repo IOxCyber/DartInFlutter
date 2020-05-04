cf5a319eb5fff94d83aaa4624dbe899972e234af
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2020-05-04 14:06:23 +0530

    introducing future & Completing an Error

--- futureIN.dart
+++ futureIN.dart
@@ -1,32 +1,38 @@
-import 'dart:async';
-main() {
-  // Passing a callback to then() will invoke
-  // that callback when the future completes
-  onReady.then((String status) {
-    print(status);
-  });
-  // Futures can be chained:
-  onReady
-      .then(print)
-      .then((_) => print('done!'));
-  // Futures can throw errors:
-  onReady.catchError(() {
-    print('error!');
-  });
-}
-Future get onReady {
-  var dur = new Duration(seconds: 1);
-  var oneSecond = new Future.delayed(dur);
-  // then() returns a new future that completes
-  // with the value of the callback.
-  return oneSecond.then((_) {
-    return 'loaded!';
-  });
+/*
+1.future(small f) is an instance of Future(Capital) Class & represent the result of an 
+asynchronous operation(Allows other operations to be completed before itself)
+& can have 2 states i.e completed and uncompleted.
+
+2. A Future<T> instance produces a value of type T.
+
+3. If a future doesn’t produce a usable value, then the future’s type is Future<void>.
+
+4. A future can be in one of two states: uncompleted or completed.
+When you call a function that returns a future, the function queues up work to be done and returns an uncompleted future.
+When a future’s operation finishes, the future completes with a value or with an error.
+
+5.
+*/
+
+//Introducing futures
+Future<void> fetchUserOrder() { //it doesn’t return a usable value, fetchUserOrder() has the type Future<void>
+  // Imagine that this function is fetching user info from another service or database
+  return Future.delayed(Duration(seconds: 5), () => print('User Data Fetched'));
 }
 
-/*
-Future<T> class. An object representing a delayed computation. 
-A Future is used to represent a potential value, or error, 
-that will be available at some time in the future. 
-Receivers of a Future can register callbacks that handle the value or error once it is available.
-*/
+void main() {
+  fetchUserOrder();
+  print('Fetching user order...');
+}
+
+//completing an Error
+Future<void> fetchUserOrder() {
+// Imagine that this function is fetching user info but encounters a bug
+  return Future.delayed(Duration(seconds: 3), () => throw Exception('Logout failed: user ID is invalid'));
+}
+
+void main() {
+  fetchUserOrder();
+  print('Fetching user order...');
+}
+
