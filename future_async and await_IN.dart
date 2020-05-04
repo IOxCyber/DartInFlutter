4bb8d4190438b72dac386b4dca3a6a0d571fe30e
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2020-05-04 23:59:15 +0530

    Update future_async and await_IN.dart

--- future_async and await_IN.dart
+++ future_async and await_IN.dart
@@ -1,1 +1,24 @@
+/*
+1.The async and await keywords provide a declarative way to define asynchronous functions and use their results. 
+Remember these two basic guidelines when using async and await:
+2.
+*/
+void main() async { ··· } //it converts main() from a synchronous to asynchronous function.
+Future<void> main() async { ··· } // T is the type of the value that the function returns here it is void.
+print(await createOrderMessage()); //await keyword to wait for a future to complete:
+Future<String>  createOrderData() async { //uses async for async functions
+  var data = await fetchUserData(); 
+  return 'Your order is: $data';
+}
 
+Future<String>  fetchUserData() =>
+    // Imagine that this function is
+    // more complex and slow.
+    
+    Future.delayed(Duration(seconds: 10), //it will wait for 10 second to return future's user data
+                   () => 'Your Fetched Data',);
+
+Future<void>  main() async {
+  print('Fetching user order...');
+  print(await createOrderData());
+}
