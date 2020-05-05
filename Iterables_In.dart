5e51ba1f4153bee82d5e7bd0de59a97f538562d9
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2020-05-06 00:24:44 +0530

    Update Iterables_In.dart

--- Iterables_In.dart
+++ Iterables_In.dart
@@ -1,1 +1,5 @@
-
+/*
+An Iterable is a collection of elements that can be accessed sequentially. In Dart, 
+an Iterable is an abstract class, meaning that you can't instantiate it directly.
+and you can create a new Iterable by creating a new List or Set .
+*/
