2842ba1746cbbb7e02ae201f77aaccfcfbd7d9e8
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2020-05-06 23:20:59 +0530

    Update Iterator_In.dart

--- Iterator_In.dart
+++ Iterator_In.dart
@@ -1,7 +1,9 @@
 /*
 An interface for getting items, one at a time, from an object.
 The for-in construct transparently uses Iterator to test for the end of the iteration, and to get each item (or element).
-
+An iterable provides a means to traverse items some sort of a list in a sequential fashion.
+List is a workhorse and you'll find it being your go-to more often than not.
+But it's certainly worth knowing about others such as Set, Map, and Queue.
 */
 
 
