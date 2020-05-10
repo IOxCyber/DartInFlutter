02741743391c1ff10fc7ecb48c8eae1b32ee023f
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2020-05-10 23:40:12 +0530

    Update and rename new.dart to Mapping_List_IN.dart

---
+++ Mapping_List_IN.dart
@@ -0,0 +1,19 @@
+class Person 
+{
+  String firstName;
+  String lastName;
+  
+  Person(this.firstName, this.lastName); //constractor in dart style
+}
+
+void main() {
+	List<Person> people = new List<Person>(); //creating object of list of Person class type
+  people.add(new Person("Joe", "Smithers"));
+  people.add(Person("Patrick", "Thomas"));//New Keyword is optional
+  var mappedNames = people.map((n) => 'Mr. ${n.firstName} ${n.lastName}'); //mapping with map() methed
+  log(mappedNames);
+}
+  
+void log(var lst) {
+  lst.forEach((n) => print(n));
+}
