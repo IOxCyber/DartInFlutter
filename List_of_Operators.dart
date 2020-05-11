89845474958bdd5d632a8b7136bf6a151e1653f9
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2020-05-12 00:13:26 +0530

    Create List_of_Operators.dart

---
+++ List_of_Operators.dart
@@ -0,0 +1,13 @@
+main() {
+  var operand1 = 10;
+  var operand2 = 7;
+
+  print(operand1 + operand2); //17
+  print(operand1 - operand2); //3
+  print(- operand1); //10
+  print(operand1 * operand2);//70
+  print(operand1 / operand2);//1.4285714285714286
+  //Divides the first operand by the second operand and returns an integer value
+  print(operand1 ~/ operand2);//1
+  print(operand1 % operand2);//3
+}
