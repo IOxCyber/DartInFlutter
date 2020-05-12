3670fa0dc64c2ef4f77a9c1f69c15e3b537efaec
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2020-05-12 18:57:00 +0530

    Create Relational_OperatorsIN.dart

---
+++ Relational_OperatorsIN.dart
@@ -0,0 +1,17 @@
+main() {
+  var operand1 = 10;
+  var operand2 = 7;
+  //Relational Operators
+  print(operand1 > operand2);
+  print(operand1 < operand2);
+  print(operand1 >= operand2);
+  print(operand1 <= operand2);
+  //Equality Operators
+  print(operand1 == operand2);
+  print(operand1 != operand2);
+  //We can also use equality operators on non-integer literals such as string literals
+  var operandA = 'a';
+  var operandB = 'b';
+  print(operand1 == operand2);
+  print(operand1 != operand2);  
+}
