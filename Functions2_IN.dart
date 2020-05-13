a5df7610c955a959900f4b7c1074669a7a7733ad
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2020-05-13 23:23:58 +0530

    Create Functions2_IN.dart

---
+++ Functions2_IN.dart
@@ -0,0 +1,18 @@
+/*
+We can divide functions into two broad categories:
+1.User-defined functions are functions that users create themselves.
+
+2.Built-in functions are functions that are predefined by Dart and are part of their libraries. 
+All we have to do to use them is call their function name.
+
+
+*/
+
+main() {
+/*
+In the code above, s1 is the object we are calling the method indexOf on and ll is the argument we are passing it. 
+The output is 2 as the starting index of ll is located at the 2nd index in the string “Hello World”.
+*/
+  String s1 = "hello";
+  print(s1.indexOf("ll"));
+}
