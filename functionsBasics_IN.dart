d6e8a78e034f2bfa541f1a0cbb39d44c9e40031b
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2020-05-18 22:36:39 +0530

    Create functionsBasics_IN.dart

---
+++ functionsBasics_IN.dart
@@ -0,0 +1,8 @@
+//shorthand type of function
+//Syntex: returntype functionName(parameters) => function body
+num sum(num x, num y) => x+y;
+
+// Driver Code
+main() {
+    print(sum(1,2)); //num type can accept both type of value float or integer.
+}
