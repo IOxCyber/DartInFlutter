65674f84e551a68ba5d2d301505ddc9e08215691
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2020-05-20 16:21:03 +0530

    Update Higher-order-functionIN.dart

--- Higher-order-functionIN.dart
+++ Higher-order-functionIN.dart
@@ -1,13 +1,16 @@
 /*
 1. Dart is a true object-oriented language, so even functions are objects and have a type {Function}. 
-2. Functions are treated like {first-class values}. What this means is that like any other value, a function can be assigned to variables, passed as a parameter to another function, and can also be returned as a result.
-3. As Function is another type just like num or List, we can create functions which have parameters of type Function. This means when calling that created function, you must pass it another function as an argument.
+2. Functions are treated like {first-class values}. What this means is that like any other value,
+   a function can be assigned to variables, passed as a parameter to another function, and can also be returned as a result.
+3. As Function is another type just like num or List, we can create functions which have parameters of type Function.
+   This means when calling that created function, you must pass it another function as an argument.
 */
 
 List<int> forAll(Function f, List<int> intList){//accept function,list as argument 
   var newList = List<int>();
   for(var i = 0; i < intList.length; i ++){
-    newList.add(f(intList[i]));//first i=0 i.e 1 from list tester[1,2,3] will be pass as argument in function f i.e controls goes to factorial function  & after calculated added to the newList.
+    newList.add(f(intList[i]));/*first i=0 i.e 1 from list tester[1,2,3] will be pass as argument in function {f} i.e 
+    controls goes to factorial function  & after calculated,result will be added to the newList.*/
   }
   return newList; //print this list i.e [1,2,6]
 }
