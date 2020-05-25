c2e1cd32ec4c27cac8701af280a988b782a959d0
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2020-05-25 18:23:28 +0530

    Create TheoryQuizONExploringFunction.dart

---
+++ TheoryQuizONExploringFunction.dart
@@ -0,0 +1,24 @@
+1.what is optional when defining a basic function?
+Ans. parameter type and return type of the function.
+
+2.
+Is the below function declaration using named parameters or positional parameters?
+
+String param(String a, int b,  [num c]) {
+}
+Ans. positional paramerters
+
+3.What does it mean for a function to be treated as a first-class value?
+Ans. 
+A function can be passed as a parameter,
+A function can be returned as a result,
+A function is treated like a basic data value such as an integer.
+
+4.
+The code snippets below are anonymous functions which return the product of two numbers.
+Which of the following is the correct implementation?
+Ans. (x,y) { x*y; } ; //Anonymous Function
+
+5.What is syntactic sugar?
+Ans. Syntax that makes code easier to read and express
+  
